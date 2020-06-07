f24fae62efb5d24c0496d63e8eabde7a9ce08529
Alex Boostrom <55629598+New2Science@users.noreply.github.com>
2020-06-06 19:46:13 -0500

    Update birthday_card.js

--- birthday_card.js
+++ birthday_card.js
@@ -33,25 +33,27 @@ window.addEventListener("click", function resizeBalloon(event) {
       	    // Show paused UI.
     	    });
   	    }
-        kidsFlying.style.marginTop = startKidsTop;
-        kidsFlying.style.marginLeft = startKidsLeft;
-        balloon.style.visibility = "hidden";
-        explosion.style.visibility = "visible";
-        balloon.style.cursor = "default";
-        tryAgain.style.visibility = "visible";
-        window.removeEventListener("click", resizeBalloon);
-        window.addEventListener("click", event => {
-          if (event.target === tryAgain) {
-            tryAgain.style.visibility = "hidden";
-            balloon.style.visibility = "visible";
-            balloon.style.cursor = "pointer";
-            explosion.style.visibility = "hidden";
-            balloon.style.width = "40px"
-            balloon.style.marginTop = startBalloonTop;
-            event.preventDefault();
-            window.addEventListener("click", resizeBalloon);
-          }
-        });
+        let delay = setTimeout(() => {
+          kidsFlying.style.marginTop = startKidsTop;
+          kidsFlying.style.marginLeft = startKidsLeft;
+          balloon.style.visibility = "hidden";
+          explosion.style.visibility = "visible";
+          balloon.style.cursor = "default";
+          tryAgain.style.visibility = "visible";
+          window.removeEventListener("click", resizeBalloon);
+          window.addEventListener("click", event => {
+            if (event.target === tryAgain) {
+              tryAgain.style.visibility = "hidden";
+              balloon.style.visibility = "visible";
+              balloon.style.cursor = "pointer";
+              explosion.style.visibility = "hidden";
+              balloon.style.width = "40px"
+              balloon.style.marginTop = startBalloonTop;
+              event.preventDefault();
+              window.addEventListener("click", resizeBalloon);
+            }
+          });
+        }, 100);  
       } else {
         let expand = new Audio("balloon_blowing_up.mp3")
         let playExpand = expand.play();
@@ -65,14 +67,16 @@ window.addEventListener("click", function resizeBalloon(event) {
             // Show paused UI.
           });
   	    }
-        balloon.style.width = larger * 1.2 + "px";
-        let higherTop = parseInt(heightTop);
-        let kidsHigherTop = parseInt(kidsTop);
-        let kidsLefter = parseFloat(kidsLeft);
-        balloon.style.marginTop = higherTop - 25 + "px";
-        kidsFlying.style.marginTop = kidsHigherTop - 10 + "px";
-        kidsFlying.style.marginLeft = kidsLefter + 6 + "px";
-        event.preventDefault();
+        let delay2 = setTimeout(() => {
+          balloon.style.width = larger * 1.2 + "px";
+          let higherTop = parseInt(heightTop);
+          let kidsHigherTop = parseInt(kidsTop);
+          let kidsLefter = parseFloat(kidsLeft);
+          balloon.style.marginTop = higherTop - 25 + "px";
+          kidsFlying.style.marginTop = kidsHigherTop - 10 + "px";
+          kidsFlying.style.marginLeft = kidsLefter + 6 + "px";
+          event.preventDefault();
+        }, 100);
       }
     }
 })
